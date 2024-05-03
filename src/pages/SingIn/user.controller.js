16182dbaf971d62d8015abe016a9ea3ce8f53bb0
lipegx <Lipe_T.J@hotmail.com>
2024-05-02 21:36:45 -0300

    correcao username logado

--- src/pages/SingIn/user.controller.js
+++ src/pages/SingIn/user.controller.js
@@ -1,5 +1,6 @@
 import { Alert } from 'react-native';
 import api from '../../services/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export const handleLogin = async (username, password, navigation) => {
@@ -11,7 +12,7 @@ export const handleLogin = async (username, password, navigation) => {
       const response = await api.post('/auth/login', { username, password });
       console.log('Login Success:', response.data)
       navigation.navigate('home');
-      const userName = "NomeDoUsuarioLogado"; 
+      const userName = username; 
       AsyncStorage.setItem('userName', userName)
         .then(() => {
           console.log('Nome do usuário salvo!');
